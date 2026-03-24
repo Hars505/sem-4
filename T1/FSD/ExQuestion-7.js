480f32f9c09e33155f5f77524b3f24e8ff0d619a
Harshil Panchal <harshil505p@gmail.com>
2026-03-24 22:21:56 +0530

    Delete T1/FSD/ExQuestion-7.js

--- T1/FSD/ExQuestion-7.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-var data = [{"name":"Aman, Ram, Rahul"},{"age":["28,30,26"]},{"RollNo":["101,102,103"]}]
-fs.writeFile("myFolder1/student.txt",JSON.stringify(data) ,(err) => {if (err) throw err;else fs.readFile("myFolder1/student.txt","UTF-8",(err,data)=>{if (err) throw err; else data = JSON.parse(data); console.log(data[0].name)})})
