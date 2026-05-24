f5e3753513802831c907d707c197023bf71f9e64
Harshil Panchal <harshil505p@gmail.com>
2026-05-24 16:57:33 +0530

    Update Parent2.jsx

--- T3/FSD-2/Myapp/src/Parent2.jsx
+++ T3/FSD-2/Myapp/src/Parent2.jsx
@@ -1,30 +1,26 @@
-import Child from "./Child2";
-
 function Parent2() {
-  function New() {
-    //temporary
-  }
-  const employees=[{name:"Harshil" , salary:90000 , id:5, rating:"Good"},
-    {name:"Shrey" , salary:10000 , id:5, rating:"Good"},
-    {name:"Vrutik" , salary:5000 , id:5, rating:"Good"},
-    {name:"Dhairya" , salary:60000 , id:5, rating:"Good"},
-    {name:"Dharm" , salary:4000 , id:5, rating:"Good"},
-  ]
+  const employees = [
+    { name: "Harshil", salary: 90000, id: 1, rating: "Good" },
+    { name: "Shrey", salary: 10000, id: 2, rating: "Good" },
+    { name: "Vrutik", salary: 5000, id: 3, rating: "Good" },
+    { name: "Dhairya", salary: 60000, id: 4, rating: "Good" },
+    { name: "Dharm", salary: 4000, id: 5, rating: "Good" },
+  ];
   return (
     <>
       <h1>Employee Details</h1>
-      {
-        employees.filter((b)=>salary==10000).map((a)=>(
-            <div>
-                <br />
-                <h2>{item.name}</h2>
-                <h2>{item.id}</h2>
-                <h2>{item.ratzing}</h2>
-                <h2>{item.seller4}</h2>
-          <br />
-            </div>
-        ))
-      }
+      {employees
+        .filter((b) => b.salary == 10000)
+        .map((a) => (
+          <div>
+            <br />
+            <h2>{a.name}</h2>
+            <h2>{a.id}</h2>
+            <h2>{a.rating}</h2>
+            <h2>{a.salary}</h2>
+            <br />
+          </div>
+        ))}
     </>
   );
 }
