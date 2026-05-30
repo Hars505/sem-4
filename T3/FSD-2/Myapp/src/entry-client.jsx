b8e6e380f420730011e27a5d37d8e74bcfbea9c7
Harshil Panchal <harshil505p@gmail.com>
2026-05-30 09:03:20 +0530

    Update entry-client.jsx

--- T3/FSD-2/Myapp/src/entry-client.jsx
+++ T3/FSD-2/Myapp/src/entry-client.jsx
@@ -6,10 +6,16 @@ import File1 from "./File1";
 import Lmn from "./Lmn";
 import Form from "./Form";
 import Parent from "./Parent";
+import Parent2 from "./Parent2";
+import Route from "./Routing";
+import { BrowserRouter } from "react-router-dom";
+import Click from "./Click";
 
 hydrateRoot(
   document.getElementById("root"),
   <StrictMode>
-    <Parent />
+    <BrowserRouter>
+      <Click />
+    </BrowserRouter>
   </StrictMode>,
 );
